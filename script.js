eca86da1104417332d7ec38f60aa051cba3c8421
GitMax76 <massi.sabato@gmail.com>
2025-02-09 19:22:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ class Game {
     }
 
     loadGitHubCSV() {
-        const githubRepo = "https://raw.githubusercontent.com/GitMax76/4Dadi/main/";
+        const githubRepo = "https://raw.githubusercontent.com/GitMax76/4Dadi\";
 
         fetch(githubRepo + 'recipes.csv')
           .then(response => response.text())
